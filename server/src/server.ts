321e733ee8a4343ae0ad34f0ba56c184e0ec003a
furPedroRibeiro <pedrocodejob@gmail.com>
2023-12-19 20:10:09 -0300

    front-end iniciado no web

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,6 +13,7 @@ app.get('/ads', (request, response) => {
         { id: 3, name: 'Anúncio 3'},
         { id: 4, name: 'Anúncio 4'},
         { id: 5, name: 'Anúncio 5'},
+        { id: 6, name: 'Anúncio 6'},
     ])
 })
 
